000679c73ab82ea9b827777737f9a7f0109bfa8d
현호 이 <dlgusgh7870@naver.com>
2021-12-13 00:52:11 +0900

    Revert "Day-2 X,Y value Fragment"
    This reverts commit ebc472f6

--- app/src/main/java/com/example/mappingmovebutton/MainActivity.java
+++ app/src/main/java/com/example/mappingmovebutton/MainActivity.java
@@ -1,26 +1,14 @@
 package com.example.mappingmovebutton;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 public class MainActivity extends AppCompatActivity {
-
-    TouchxyFragment touchxyFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        touchxyFragment = new TouchxyFragment();
-
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-
-        fragmentTransaction.add(R.id.container,touchxyFragment);
-        fragmentTransaction.commit();
     }
 }
